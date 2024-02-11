d6b286b8794b81e009e097b7f3f28860861fc563
Rohit Goswami <rog32@hi.is>
2024-02-11 22:32:57 +0000

    ENH: Add an example of parametrization

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,18 +1,38 @@
-from asv_samples.benchme import add_arr
+import numpy as np
+from asv_runner.benchmarks.mark import parameterize, SkipNotImplemented
+from asv_samples.benchme import NUMPY_AVAILABLE
 
+if not NUMPY_AVAILABLE:
+    raise SkipNotImplemented("Can't run without NumPy")
 
-class TimeSuite:
-    """
-    Benchmark that times various operations, including custom summation of
-    lists.
-    """
+@parameterize({"n":[10, 100]})
+def time_sort(n):
+    np.sort(np.random.rand(n))
 
-    def setup(self):
-        self.list1 = [i for i in range(500)]
-        self.list2 = [i for i in range(500, 1000)]
+@parameterize({'n': [10, 100], 'func_name': ['range', 'arange']})
+def time_ranges_multi(n, func_name):
+    f = {'range': range, 'arange': np.arange}[func_name]
+    for i in f(n):
+        pass
 
-    def time_add_arr(self):
-        """
-        Time the add_arr function with two lists of numbers.
-        """
-        add_arr(self.list1, self.list2)
+@parameterize({"size": [10, 100, 200]})
+class TimeSuiteDecoratorSingle:
+    def setup(self, size):
+        self.d = {}
+        for x in range(size):
+            self.d[x] = None
+
+    def time_keys(self, size):
+        for key in self.d.keys():
+            pass
+
+    def time_values(self, size):
+        for value in self.d.values():
+            pass
+
+@parameterize({'n': [10, 100], 'func_name': ['range', 'arange']})
+class TimeSuiteMultiDecorator:
+    def time_ranges(self, n, func_name):
+        f = {'range': range, 'arange': np.arange}[func_name]
+        for i in f(n):
+            pass
