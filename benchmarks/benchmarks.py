aba59c70123d8128a027c9aea2beac55d069d440
Rohit Goswami <rog32@hi.is>
2024-02-11 11:55:14 +0000

    BENCH: Add some basic timing benchmarks

---
+++ benchmarks/benchmarks.py
@@ -0,0 +1,15 @@
+from benchme import add_arr
+
+class TimeSuite:
+    """
+    Benchmark that times various operations, including custom summation of lists.
+    """
+    def setup(self):
+        self.list1 = [i for i in range(500)]
+        self.list2 = [i for i in range(500, 1000)]
+
+    def time_add_arr(self):
+        """
+        Time the add_arr function with two lists of numbers.
+        """
+        add_arr(self.list1, self.list2)
